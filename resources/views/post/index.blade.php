61406458539f7989499343ad2d6bc39bb11bb9d2
felvian <felvian221@gmail.com>
2023-09-26 07:59:29 +0300

    2

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -2,8 +2,14 @@
 @section('content')
     <div class="container">
         <a href="{{ route('post.create') }}" class="btn btn-primary mb-3"> add post</a>
+        @php
+          $i=1
+        @endphp
         @foreach($posts as $post)
-            <div class="mb-2"><a href="{{ route('post.show', $post->id) }}">{{ $post->id }}. {{ $post->title }}</a></div>
+            <div class="mb-2"><a href="{{ route('post.show', $post->id) }}">{{ $i }}. {{ $post->title }}</a></div>
+            @php
+              $i++
+            @endphp
         @endforeach
     </div>
 @endsection
